6fb84ba8d237bd973c192605d7a51648d48d2444
LunarOrbital <254162809+LunarOrbital@users.noreply.github.com>
2026-02-11 12:45:02 -0600

    working star expansion and color change

--- game-jam-iii/Scripts/star_template.gd
+++ game-jam-iii/Scripts/star_template.gd
@@ -3,11 +3,13 @@ class_name Star
 @onready var timer: Timer = $Timer
 var numPlanets : int
 @onready var star_mesh_temp_: MeshInstance3D = $"StarMesh(Temp)"
-@onready var star_coll: CollisionShape3D = $StarColl
+@onready var star_coll: CollisionShape3D = $starColl
 @onready var planet: Planet = $Planet
 var sizeSpd = 1.0002
 @export var spdCurve : Curve
 var startTime : int
+@onready var sun_kill_box: Area3D = $SunKillBox
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	planet.linear_velocity = Vector3(30,0,0)
@@ -15,11 +17,15 @@ func _ready() -> void:
 	timer.wait_time -= startTime
 	timer.start()
 func star_progress_ratio() -> float:
-	return 1.0 - (timer.time_left/400)
+	return 1.0-(timer.time_left/400)
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	sizeSpd = (star_progress_ratio()/500)+1
 	star_mesh_temp_.scale *=sizeSpd
 	star_coll.scale *= sizeSpd
+	sun_kill_box.scale *= sizeSpd
+	star_mesh_temp_.mesh.material.albedo_color.g -= star_progress_ratio()*(_delta*4)
+
 func _on_sun_kill_box_body_entered(body: Node3D) -> void:
 	if (body is player):
 		print("you burned up and died")
